2ec2d4e6ee4273182b01b5caab9a9e2eec9c58fd
WantToLearnMore <1740805993@qq.com>
2019-09-10 14:26:41 +0800

    Z字形变换添加官方答案

--- src/main/java/com/priactice/algorithm/checkHome/ZConvert.java
+++ src/main/java/com/priactice/algorithm/checkHome/ZConvert.java
@@ -79,6 +79,35 @@ public class ZConvert {
     }
 
 
+
+    /*
+    *思路
+    按照与逐行读取 Z 字形图案相同的顺序访问字符串。
+
+   算法
+   首先访问 行 0 中的所有字符，接着访问 行 1，然后 行 2，依此类推...
+   对于所有整数 kk，
+
+   行 0 中的字符位于索引 k(2⋅numRows−2) 处;
+   行 numRows−1 中的字符位于索引 k(2⋅numRows−2)+numRows−1 处;
+   内部的行 i 中的字符位于索引 k(2⋅numRows−2)+i 以及 (k+1)(2⋅numRows−2)−i 处;
+   **/
+    public String solution2(String s,int numRows){
+        if (numRows == 1) return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
     /*
     *思路：通过从左向右迭代字符串。
     *算法：使用min（numRows，len(s)）列表来表示Z字形图案中的非空行
